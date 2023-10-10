91f9cd7f83876ac4df7b5674a51060a73773de4d
Muzaffar khan <41264014+muzafferjoya@users.noreply.github.com>
2023-10-10 11:56:19 +0530

    Update StartApplication.java

--- src/main/java/com/khan/StartApplication.java
+++ src/main/java/com/khan/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "This is a Sample Application");
+        model.addAttribute("title", "CD ArogCD");
 
 
         return "index";
